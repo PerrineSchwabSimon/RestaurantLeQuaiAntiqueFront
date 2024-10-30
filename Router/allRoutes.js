0733db46b3b4d098ba54ec53eeb334f6fc7b1b6e
Perrine Schwab-Simon <schwabsimon.perrine01@gmail.com>
2024-10-30 18:43:56 +0100

    Ajout gest carte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,13 +4,14 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie","La galerie", "/pages/galerie.html"),
-    new Route("/carte","La carte", "/pages/carte.html"),
+    new Route("/carte","La carte", "/pages/gestcarte/carte.html"),
     new Route("/signin","Connexion", "/pages/auth/signin.html"),
     new Route("/signup","Inscription", "/pages/auth/signup.html"),
     new Route("/account","Mon compte", "/pages/auth/pageaccount.html"),
     new Route("/editPassword","Modifier le mot de passe", "/pages/auth/editpassword.html"),
     new Route("/allresa","Vos réservations", "/pages/reservations/allresa.html"),
     new Route("/editresa","Ma réservation", "/pages/reservations/editresa.html"),
+    new Route("/editcarte","Modifier la carte", "/pages/gestcarte/editcarte.html"),
     ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
