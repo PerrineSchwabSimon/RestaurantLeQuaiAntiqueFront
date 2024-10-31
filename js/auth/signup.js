6c2840371496a4de2f82c341a79fd0c93aa64c3d
Perrine Schwab-Simon <schwabsimon.perrine01@gmail.com>
2024-10-31 16:00:27 +0100

    Essai 2

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -5,6 +5,7 @@ const inputPreNom = document.getElementById("PrenomInput");
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidationPassword = document.getElementById("ValidatePasswordInput");
+const btnValidation = document.getElementById("btn-validation-inscription");
 
 inputNom.addEventListener("keyup", validateForm); 
 inputPreNom.addEventListener("keyup", validateForm);
@@ -14,17 +15,43 @@ inputValidationPassword.addEventListener("keyup", validateForm);
 
 //Function permettant de valider tout le formulaire
 function validateForm(){
-    validateRequired(inputNom);
-    validateRequired(inputPreNom);
+    const nomOk = validateRequired(inputNom);
+    const prenomOk = validateRequired(inputPreNom);
+    const mailOk = validateMail(inputMail);
+
+    if(nomOk && prenomOk && mailOk){
+        btnValidation.disabled = false;
+    }
+    else{
+        btnValidation.disabled = true;
+    }
+}
+
+function validateMail(input){
+    //Définir mon regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser= inputMail.value;
+    if(mailUser.match(emailRegex)){
+            input.classList.add("is-valid");
+            input.classList.remove("is-invalid");
+            return true;
+        }
+        else{
+            input.classList.remove("is-valid");
+            input.classList.add("is-invalid");
+            return false;       
+    }
 }
 
 function validateRequired(input){
     if(input.value != ''){
         input.classList.add("is-valid");
-        input.classList.remove("is-invalid"); 
+        input.classList.remove("is-invalid");
+        return true; 
     }
     else{
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
+        return false;
     }
 }
