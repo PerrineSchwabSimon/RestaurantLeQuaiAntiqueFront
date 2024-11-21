c729290f1cb8944a8919d62faa045799b6abff3c
Perrine <schwabsimon.perrine01@gmail.com>
2024-11-21 11:49:47 +0100

    Sanitize

---
+++ js/auth/galerie.js
@@ -0,0 +1,30 @@
+const galerieImage = document.getElementById("allImages");
+
+//Récuperer les informations des images
+let titre = '<img src=x onerror="window.location.replace(\'https://google.com\')"/>';
+let imgSource = "../Images/food-1050813_1280.jpg";
+
+let monImage = getImage(titre, imgSource);
+
+galerieImage.innerHTML = monImage;
+
+function sanitizeHtml(texte){
+    const tempHtml = document.createElement('div');
+    tempHtml.textContent = text;
+    return tempHtml.innerHTML;
+}
+
+function getImage(titre, urlImage){
+    titre = sanitizeHtml(titre);
+    urlImage = sanitizeHtml(urlImage);
+    return ` <div class="col p-3">
+                    <div class="image-card text-light">
+                        <img src="${urlImage}" class="rounded w-100"/>
+                        <p class="titre-image">${titre}</p>
+                        <div data-show="admin" class="action-image-buttons">
+                            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#EditionPhotoModal"><i class="bi bi-pencil-square"></i></button>
+                            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#DeletePhotoModal"><i class="bi bi-trash"></i></button>
+                        </div>
+                    </div>
+                </div>`;
+}
